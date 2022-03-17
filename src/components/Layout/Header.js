dd953cb94b79082a21265544c93c916ad2344594
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-03-17 19:43:34 +0530

    added button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,19 @@
- import { Fragment } from 'react';
+import { Fragment } from 'react';
 import classes from './Header.module.css';
-import meals from '../../assests/meals.jpg'
- const Header= props =>{
-     return(
-         <Fragment>
-             <header className={classes.header}>
-                 <h1> React Meals </h1>
-                  <button>Cart</button>  
+import meals from '../../assests/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+const Header = props => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1> React Meals </h1>
+                <HeaderCartButton />
             </header>
-         <div className={classes['main-image']}>
-<img src={meals}/>
-         </div>
-         </Fragment>
-     )
+            <div className={classes['main-image']}>
+                <img src={meals} />
+            </div>
+        </Fragment>
+    )
 
 }
 export default Header;
