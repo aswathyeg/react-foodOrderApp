53ab203de3dcd78dfb6379c2c0c4ca7f84d7a98c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-03-19 01:09:45 +0530

    added MealItem

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,6 +2,7 @@
   
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
+import MealItem from './MealItem';
  
  const DUMMY_MEALS = [
     {
@@ -30,7 +31,8 @@ import Card from '../UI/Card';
     },
   ];
   const AvailableMeals = (props) => {
-    const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal) => <MealItem  key={meal.id} name={meal.name}
+    description={meal.description} price={meal.price}/>);
   
     return (
       
