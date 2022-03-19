53c35dd1d038e214f5c5d338de494dcf870059d1
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-03-19 23:41:32 +0530

    mealitem added

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 const MealItem=(props)=>{
     const price=`$${props.price.toFixed(2)}`
     return(
@@ -12,6 +13,7 @@ const MealItem=(props)=>{
         <div className={classes.price}>
 {price}
         </div>
+        <MealItemForm/>
         </li>
     )
 
