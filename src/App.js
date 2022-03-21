25949f31c4d3ac383a2d02a914e0850631f178ce
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-03-21 22:00:41 +0530

    added event handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [showCart,setShowCart]=useState(false);
+  const handleShowCart=()=>{
+    setShowCart(true);
+
+  }
+  const handleHideCart=()=>{
+    setShowCart(false);
+
+  }
+
   return (
    <Fragment>
-     <Header/>
+     {showCart && <Cart onClose = {handleHideCart}/>}
+     
+     <Header handleShowCart={handleShowCart}/>
      <main>
        <Meals/>
      </main>
