cee3bba3786ad8ca28ee4eeb992fd0ea3b7992b8
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-03-29 14:41:35 +0530

    items added to context

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,7 +1,20 @@
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../store/cart-context';
+import { useContext } from 'react';
+
 const MealItem=(props)=>{
-    const price=`$${props.price.toFixed(2)}`
+   const cartCtx =  useContext(CartContext);
+    const price=`$${props.price.toFixed(2)}`;
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id :props.id,
+            name : props.name,
+            amount : amount,
+            price : props.price
+        })
+
+    }
     return(
         <li className={classes.meal}>
         <div>
@@ -13,7 +26,7 @@ const MealItem=(props)=>{
         <div className={classes.price}>
 {price}
         </div>
-        <MealItemForm/>
+        <MealItemForm onAddToCart = {addToCartHandler}/>
         </li>
     )
 
